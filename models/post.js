b1accf20a8b906d779f755e8ed7653b08dd5ddfd
Kartik Srikumar <kartik.srikumar@gmail.com>
2023-03-31 12:56:01 -0500

    added date to model and to the viewOne page

--- models/post.js
+++ models/post.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {type: String, required:true},
     //author: String,
-    body: {type: String, required:true}
+    body: {type: String, required:true},
     //story: {type: String, required:false},
     //published: {type: String, date: Date},
     //comments: {type: String, required:false},
-    //date: {type: Date, default: Date.now}
+    date: {type: Date, default: Date.now}
    });
 
    const Post = mongoose.model('Post', postSchema);
